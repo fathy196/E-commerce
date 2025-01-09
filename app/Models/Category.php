1b872c6569ce3c1c146b5d3d9096320693f83ad9
ziadadel001 <ziadadel00120@gmail.com>
2025-01-09 14:33:10 +0200

    define the relations bettween product and category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $table = 'categories';
+
+    protected $fillable = [
+        'name',
+        'description',
+    ];
+
+    //*  the relationship with the Product model
     public function products()
     {
         return $this->hasMany(Product::class);
--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,8 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $table = 'products';
 
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'quantity',
+        'image',
+        'category_id',
+        'is_active',
+    ];
+
+    //* Define the relationship with the Category model
     public function category()
     {
         return $this->belongsTo(Category::class);
